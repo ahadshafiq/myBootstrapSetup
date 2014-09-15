03da0c63af0404444d24880b66164129942d3eae
Ahad Shafiq <ahad.shafiq@gmail.com>
2014-09-15 16:56:26 -0300

    heroku deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,24 @@ module.exports = function (grunt) {
         ]
       }
     },
+    copy: {
+        dist: {
+            files: [{
+                expand: true,
+                dest: '<%= yeoman.dist %>',
+                cwd: 'heroku',
+                src: '*',
+                rename: function (dest, src) {
+                    var path = require('path');
+                    if (src === 'distpackage.json') {
+                        return path.join(dest, 'package.json');
+                    }
+                    return path.join(dest, src);
+                }
+            }]
+        }
+    },
+
 
     // The actual grunt server settings
     connect: {
