c568faf7d84fc09a2ae5156b623330cf34ec9648
Ahad Shafiq <ahad.shafiq@gmail.com>
2014-09-15 17:07:19 -0300

    got rid of yeoman heroku generator shit

--- Gruntfile.js
+++ Gruntfile.js
@@ -384,21 +384,4 @@ module.exports = function (grunt) {
   ]);
 };
 
-copy: {
-        dist: {
-            files: [{
-                expand: true,
-                dest: '<%= yeoman.dist %>',
-                cwd: 'heroku',
-                src: '*',
-                rename: function (dest, src) {
-                    var path = require('path');
-                    if (src === 'distpackage.json') {
-                        return path.join(dest, 'package.json');
-                    }
-                    return path.join(dest, src);
-                }
-            }]
-        }
-    };
 
